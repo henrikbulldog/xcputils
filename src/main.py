aa11fd9dc3ef2b68250dc86a29937f24e9ac2b1a
Henrik Thomsen <xhtho@energinet.dk>
2023-03-22 14:08:51 +0000

    main

--- src/main.py
+++ src/main.py
@@ -2,23 +2,19 @@
     https://docs.databricks.com/dev-tools/databricks-connect.html#access-dbutils """
 
 import os
-from xcputils import xcputils
 
-spark = SparkSession.builder.getOrCreate()
-xcputils = xcputils()
+from xcputils import XCPUtils
+
+xcputils = XCPUtils()
 
 
 bucket_name = os.getenv("AWS_S3_BUCKET", "My-bucket")
 folder = "bronze/eds"
 file_name = "co2emis.json"
 
-connector = xcputils.create_aws_s3_stream_connector(
-    bucket_name=bucket_name,
-    file_path=f"{folder}/{file_name}")
+connector = xcputils.create_string_stream_connector()
 
-xcputils.ingestion.http.get(url="https://api.energidataservice.dk/dataset/CO2Emis",
-    connector=connector,
-    params={"start": "2022-01-01T00:00", "end": "2022-01-01T01:00"})
+xcputils.http.get(url="https://postman-echo.com/ip",
+    connector=connector)
 
-s3_path = f"s3://{bucket_name}/{folder}"
-
+print(connector.read_str())
